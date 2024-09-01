6665c03a4183fc44567de8344bcbc9d6feda4865
khengarradhika <khengarradhika5@gmail.com>
2024-09-01 14:49:33 +0530

    Add test for adding books

---
+++ test/library.test.js
@@ -0,0 +1,8 @@
+const Library = require('../library');
+
+test('should add a book to the library', () => {
+  const library = new Library();
+  const book = { isbn: '210280116001', title: 'JavaScript Essentials', author: 'Radhika Khengar', year: 2024 };
+  library.addBook(book);
+  expect(library.getBooks()).toContainEqual(book);
+});
